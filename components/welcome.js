9f04f774e7920b3741eb77fb08584086250e88a3
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-10-10 20:52:23 -0500

    created landing page

---
+++ components/welcome.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native'
+
+
+function welcome() {
+    return (
+        <View>
+            <Text> Hello friend, welcome to my Doggie app! </Text> 
+        </View>
+       
+    )
+}
+
+export default welcome
